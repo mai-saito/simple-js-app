ff5cd63319e680e52140c8ae29fb975c07f85c18
Mai Saito <mai.s@hotmail.co.jp>
2019-05-30 22:01:28 +0100

    Update script.js
    Added array and loop for the pokemon

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert('Hello World');
+var repository =[
+  ['Bulbasaur',
+  0.7,
+  ['Grass', 'Poison']],
+  ['Charmander',
+  0.6,
+  ['Fire']],
+  ['Squirtle',
+  0.5,
+  ['Water']]
+];
+
+for (var i = 0; i < repository.length; i++){
+  if (repository[i][1] > 0.6) {
+  document.write(repository[i][0] + ' (height: ' + repository[i][1] + ') Wow, that\'s big!');
+} else {
+  document.write(repository[i][0] + ' (height: ' + repository[i][1] + ')');
+}
+}
