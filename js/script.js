5a7188a1c7af448e08bd6b406230a50a09b0ed7f
Mai Saito <mai.s@hotmail.co.jp>
2019-06-08 22:10:05 +0100

    Changed forEach function
    Changed loop to forEach function to show pokemon's name and height.

--- js/script.js
+++ js/script.js
@@ -16,11 +16,11 @@ var repository =[
   }
 ];
 
-for (var i = 0; i < repository.length; i++){
-    var pokemon = repository[i];
+
+repository.forEach(function(pokemon){
   if (pokemon.height > 0.6) {
-    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') Wow, that\'s big!' +'</p>');
-  } else {
-    document.write('<p>'+pokemon.name + ' (height: ' + pokemon.height + ')'+'</p>');
-  }
-}
+   document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') Wow, that\'s big!' +'</p>');
+ } else {
+   document.write('<p>'+pokemon.name + ' (height: ' + pokemon.height + ')'+'</p>');
+ }
+});
