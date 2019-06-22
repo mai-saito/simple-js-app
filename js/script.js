b178b31516c554759d37308208d6ce75e51eb3de
Mai Saito <mai.s@hotmail.co.jp>
2019-06-22 19:47:14 +0100

    Changed addListItem function

--- js/script.js
+++ js/script.js
@@ -1,50 +1,34 @@
 var pokemonRepository = (function(){
-var repository =[
-  {
-    name:'Bulbasaur',
-    height: 0.7,
-    types:['Grass', 'Poison']
-  },
-  {
-    name:'Charmander',
-    height: 0.6,
-    types:['Fire']
-  },
-  {
-    name:'Squirtle',
-    height: 0.5,
-    types:['Water']
+  var repository =[
+    {
+      name:'Bulbasaur',
+      height: 0.7,
+      types:['Grass', 'Poison']
+    },
+    {
+      name:'Charmander',
+      height: 0.6,
+      types:['Fire']
+    },
+    {
+      name:'Squirtle',
+      height: 0.5,
+      types:['Water']
+    }
+  ];
+
+  function getAll(){
+    return repository;
   }
-];
 
-function getAll(){
-  return repository;
-}
+  function add(item){
+    repository.push(item);
+  }
 
-function add(item){
-  repository.push(item);
-}
-
-
-function addListItem(pokemon){
-  var listItem = document.createElement('li');
-  var button = document.createElement('button');
-  button.innerText = pokemon.name;
-  listItem.appendChild(button);
-  $pokemonList.appendChild(listItem);
-  button.classList.add('pokemon-list_style');
-  $pokemonList.addEventListener('click', showDetails());
-}
-
-function showDetails(pokemon){
-  console.log(pokemon);
-}
-
-return {
-   add: add,
-   getAll: getAll,
-   addListItem: addListItem
- };
+  return {
+    add: add,
+    getAll: getAll,
+  };
 })();
 
 pokemonRepository.add({
@@ -55,6 +39,20 @@ pokemonRepository.add({
 
 var $pokemonList = document.querySelector('.pokemon-list');
 
-pokemonRepository.getAll().forEach(function(pokemon){
-  return pokemonRepository.addListItem(pokemon);
-});
+function addListItem(pokemon){
+  var listItem = document.createElement('li');
+  var button = document.createElement('button');
+  button.innerText = pokemon.name;
+  listItem.appendChild(button);
+  $pokemonList.appendChild(listItem);
+  button.classList.add('pokemon-list_style');
+  button.addEventListener('click', function(){
+    showDetails(pokemon)
+  });
+}
+
+function showDetails(pokemon){
+  console.log(pokemon);
+}
+
+pokemonRepository.getAll().forEach(addListItem);
